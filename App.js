03c3820efcac2fc782f8f11217ed1ba3c312fa1b
Roman Serediuk <box4rom@gmail.com>
2023-02-07 18:59:49 +0200

    add fonts

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
+
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+
 import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return undefined;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <>
-      <RegistrationScreen />
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
       <StatusBar style="auto" />
     </>
   );
