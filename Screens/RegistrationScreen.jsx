05732c44d8f72c51f42ca1bb67464e495638dac0
Roman Serediuk <box4rom@gmail.com>
2023-02-07 19:04:06 +0200

    add keybord handling logic, add styles

--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -5,94 +5,172 @@ import {
   Text,
   View,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   TextInput,
+  // KeyboardAvoidingView,
+  Keyboard,
 } from "react-native";
 
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
 export default function RegistrationScreen() {
-  const initialState = {
-    login: "",
-    email: "",
-    password: "",
-  };
+  const [credentials, setCredentials] = useState(initialState);
+  const [isShownKeyboard, setIsShownKeyboard] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
   const inputHandler = (value, name) => {
     setCredentials((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const [credentials, setCredentials] = useState(initialState);
+  const onFocusHandler = () => {
+    setIsShownKeyboard(true);
+  };
+
+  const keyboardHide = () => {
+    setIsShownKeyboard(false);
+    Keyboard.dismiss();
+  };
+
+  const submitHandler = () => {
+    console.log(credentials);
+    setCredentials(initialState);
+  };
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../assets/images/background_2x.jpg")}
-        style={styles.image}
-      >
-        <View style={styles.form}>
-          <Text style={styles.title}>Registration</Text>
-          <TextInput
-            placeholder="Login"
-            placeholderTextColor="#BDBDBD"
-            textAlign="left"
-            value={credentials.login}
-            onChangeText={(value) => {
-              inputHandler(value, "login");
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={require("../assets/images/background_2x.jpg")}
+          style={styles.image}
+        >
+          {/* <KeyboardAvoidingView
+            behavior={Platform.OS == "ios" ? "padding" : "hight"}
+          > */}
+          <View
+            style={{
+              ...styles.form,
+              paddingBottom: isShownKeyboard ? 20 : 45,
             }}
-            style={styles.textInput}
-          />
-          <TextInput
-            placeholder="Email"
-            placeholderTextColor="#BDBDBD"
-            textAlign="left"
-            value={credentials.email}
-            onChangeText={(value) => {
-              inputHandler(value, "email");
-            }}
-            style={styles.textInput}
-          />
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor="#BDBDBD"
-            textAlign="left"
-            value={credentials.password}
-            secureTextEntry={true}
-            onChangeText={(value) => {
-              inputHandler(value, "password");
-            }}
-            style={styles.textInput}
-          />
-          <TouchableOpacity activeOpacity={0.8} style={styles.registerBtn}>
-            <Text style={styles.registerBtnText}>Sign up</Text>
-          </TouchableOpacity>
-        </View>
-      </ImageBackground>
-    </View>
+          >
+            <Text style={styles.title}>Registration</Text>
+            <TextInput
+              placeholder="Login"
+              placeholderTextColor="#BDBDBD"
+              textAlign="left"
+              value={credentials.login}
+              onFocus={() => {
+                onFocusHandler();
+                setIsFocused(true);
+              }}
+              onBlur={() => setIsFocused(false)}
+              onChangeText={(value) => {
+                inputHandler(value, "login");
+              }}
+              // style={styles.textInput}
+              style={{
+                ...styles.textInput,
+                borderColor: isFocused ? "#FF6C00" : "#E8E8E8",
+                backgroundColor: isFocused ? "#FFFFFF" : "#F6F6F6",
+              }}
+            />
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor="#BDBDBD"
+              textAlign="left"
+              value={credentials.email}
+              onFocus={() => {
+                onFocusHandler();
+                setIsFocused(true);
+              }}
+              onBlur={() => setIsFocused(false)}
+              onChangeText={(value) => {
+                inputHandler(value, "email");
+              }}
+              // style={styles.textInput}
+              style={{
+                ...styles.textInput,
+                borderColor: isFocused ? "#FF6C00" : "#E8E8E8",
+                backgroundColor: isFocused ? "#FFFFFF" : "#F6F6F6",
+              }}
+            />
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor="#BDBDBD"
+              textAlign="left"
+              value={credentials.password}
+              secureTextEntry={true}
+              onFocus={() => {
+                onFocusHandler();
+                setIsFocused(true);
+              }}
+              onBlur={() => setIsFocused(false)}
+              onChangeText={(value) => {
+                inputHandler(value, "password");
+              }}
+              // style={styles.textInput}
+              style={{
+                ...styles.textInput,
+                borderColor: isFocused ? "#FF6C00" : "#E8E8E8",
+                backgroundColor: isFocused ? "#FFFFFF" : "#F6F6F6",
+              }}
+            />
+            {!isShownKeyboard && (
+              <>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  onPress={submitHandler}
+                  style={styles.registerBtn}
+                >
+                  <Text style={styles.registerBtnText}>Sign up</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={0.8}
+                  onPress={() => console.log("Click")}
+                  style={styles.redirectBtn}
+                >
+                  <Text style={styles.redirectText}>
+                    Already have an account? Log in
+                  </Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+          {/* </KeyboardAvoidingView> */}
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
+    flex: 1,
   },
   image: {
     flex: 1,
-    resizeMode: "cover",
     justifyContent: "flex-end",
+    resizeMode: "cover",
   },
   title: {
-    // fontFamily: "Roboto-Medium",
+    color: "#212121",
+    fontFamily: "Roboto-Medium",
     fontSize: 30,
     lineHeight: 35,
     letterSpacing: 0.16,
     marginBottom: 32,
     marginTop: 60,
     textAlign: "center",
-    color: "#212121",
   },
   form: {
     backgroundColor: "#FFFFFF",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
+    fontFamily: "Roboto-Regular",
     paddingTop: 32,
     paddingLeft: 16,
     paddingRight: 16,
@@ -100,31 +178,37 @@ const styles = StyleSheet.create({
   textInput: {
     borderWidth: 1,
     borderRadius: 8,
-    borderColor: "#E8E8E8",
-    backgroundColor: "#F6F6F6",
-    width: "100%",
-    padding: 15,
-    marginBottom: 16,
-
-    // font-family: 'Roboto';
-    // fontWeight: 400,
-    fontSize: 16,
-    lineHeight: 19,
     color: "#212121",
+    fontSize: 16,
+    // borderColor: "#E8E8E8",
+    // backgroundColor: "#F6F6F6",
+    lineHeight: 19,
+    marginBottom: 16,
+    padding: 15,
+    width: "100%",
   },
   registerBtn: {
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+    marginTop: 33,
     paddingTop: 16,
     paddingBottom: 16,
-    backgroundColor: "#FF6C00",
-    borderRadius: 100,
-    marginTop: 43,
   },
   registerBtnText: {
     color: "#FFFFFF",
-    // font-family: 'Roboto';
-    // fontWeight: 400,
     fontSize: 16,
     lineHeight: 19,
+    textAlign: "center",
+  },
+  redirectBtn: {
+    marginTop: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  redirectText: {
+    color: "#1B4371",
+    fontSize: 16,
+    lineHeight: 18,
     textAlign: "center",
   },
 });
