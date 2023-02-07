04eee7496aa2689ed3538fff47c0b28f9d7c285b
Roman Serediuk <box4rom@gmail.com>
2023-02-07 11:27:48 +0200

    import RegistRegistrationScreen component

--- App.js
+++ App.js
@@ -1,29 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("./assets/images/background_2x.jpg")}
-        style={styles.image}
-      >
-        <Text>Hello motherfuckers!</Text>
-      </ImageBackground>
+    <>
+      <RegistrationScreen />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  image: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
