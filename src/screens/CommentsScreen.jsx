7c9a6b5f939427ac1920482ed8d91f01d748e12e
Roman Serediuk <box4rom@gmail.com>
2023-02-14 09:44:50 +0200

    init setup

---
+++ src/screens/CommentsScreen.jsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default CommentsScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>CommentsScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
