f01bad0e90ec9c2fa1e79f11c37e20b837840742
Roman Serediuk <box4rom@gmail.com>
2023-02-14 10:12:30 +0200

    implement keyboard hide

--- src/screens/main/CreatePostsScreen.jsx
+++ src/screens/main/CreatePostsScreen.jsx
@@ -6,9 +6,12 @@ import {
   StyleSheet,
   Image,
   TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import { Camera } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
 import { FontAwesome, SimpleLineIcons } from "@expo/vector-icons";
 
 const initialState = {
@@ -22,6 +25,12 @@ export default function CreatePostsScreen({ navigation }) {
   const [cameraRef, setCameraRef] = useState(null);
   const [photoInfo, setPhotoInfo] = useState(initialState);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const [isShownKeyboard, setIsShownKeyboard] = useState(false);
+
+  const keyboardHide = () => {
+    setIsShownKeyboard(false);
+    Keyboard.dismiss();
+  };
 
   useEffect(() => {
     (async () => {
@@ -70,100 +79,102 @@ export default function CreatePostsScreen({ navigation }) {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.photoContainer}>
-        {photo ? (
-          <>
-            <View style={styles.imageWrapper}>
-              <Image style={styles.image} source={{ uri: photo }} />
-            </View>
-            <TouchableOpacity
-              style={styles.refreshButton}
-              onPress={refreshPhoto}
-            >
-              <FontAwesome name="refresh" size={20} color="white" />
-            </TouchableOpacity>
-          </>
-        ) : (
-          <Camera
-            style={styles.camera}
-            type={type}
-            ref={(ref) => {
-              setCameraRef(ref);
-            }}
-          >
-            <View style={styles.photoView}>
-              <TouchableOpacity style={styles.button} onPress={takePhoto}>
-                <FontAwesome name="camera" size={24} color="white" />
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <View style={styles.photoContainer}>
+          {photo ? (
+            <>
+              <View style={styles.imageWrapper}>
+                <Image style={styles.image} source={{ uri: photo }} />
+              </View>
+              <TouchableOpacity
+                style={styles.refreshButton}
+                onPress={refreshPhoto}
+              >
+                <FontAwesome name="refresh" size={20} color="white" />
               </TouchableOpacity>
-            </View>
-          </Camera>
-        )}
-      </View>
-      <Text style={styles.cameraTag}>
-        {photo ? "Add photo description" : "Take a photo"}
-      </Text>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          value={photoInfo.title}
-          onChangeText={(value) => {
-            inputHandler(value, "title");
-          }}
-          textAlign="left"
-          // onFocus={}
-          // onBlur={}
+            </>
+          ) : (
+            <Camera
+              style={styles.camera}
+              type={type}
+              ref={(ref) => {
+                setCameraRef(ref);
+              }}
+            >
+              <View style={styles.photoView}>
+                <TouchableOpacity style={styles.button} onPress={takePhoto}>
+                  <FontAwesome name="camera" size={24} color="white" />
+                </TouchableOpacity>
+              </View>
+            </Camera>
+          )}
+        </View>
+        <Text style={styles.cameraTag}>
+          {photo ? "Add photo description" : "Take a photo"}
+        </Text>
+        <View style={styles.inputWrapper}>
+          <TextInput
+            value={photoInfo.title}
+            onChangeText={(value) => {
+              inputHandler(value, "title");
+            }}
+            textAlign="left"
+            // onFocus={}
+            // onBlur={}
+            style={{
+              ...styles.textInput,
+              // borderColor: isInFocus ? "#FF6C00" : "#E8E8E8",
+              // backgroundColor: isInFocus ? "#FFFFFF" : "#F6F6F6",
+            }}
+            placeholderTextColor="#BDBDBD"
+            placeholder="Title..."
+          />
+          <TextInput
+            value={photoInfo.location}
+            onChangeText={(value) => {
+              inputHandler(value, "location");
+            }}
+            textAlign="left"
+            // onFocus={}
+            // onBlur={}
+            style={{
+              ...styles.textInput,
+              marginTop: 16,
+              paddingLeft: 30,
+              // borderColor: isInFocus ? "#FF6C00" : "#E8E8E8",
+              // backgroundColor: isInFocus ? "#FFFFFF" : "#F6F6F6",
+            }}
+            placeholderTextColor="#BDBDBD"
+            placeholder="Location..."
+          />
+          <SimpleLineIcons
+            style={styles.locationIcon}
+            name="location-pin"
+            size={24}
+            color="#BDBDBD"
+          />
+        </View>
+        <TouchableOpacity
+          disabled={!isPhotoDataReady}
+          activeOpacity={0.8}
+          onPress={postHandler}
           style={{
-            ...styles.textInput,
-            // borderColor: isInFocus ? "#FF6C00" : "#E8E8E8",
-            // backgroundColor: isInFocus ? "#FFFFFF" : "#F6F6F6",
-          }}
-          placeholderTextColor="#BDBDBD"
-          placeholder="Title..."
-        />
-        <TextInput
-          value={photoInfo.location}
-          onChangeText={(value) => {
-            inputHandler(value, "location");
-          }}
-          textAlign="left"
-          // onFocus={}
-          // onBlur={}
-          style={{
-            ...styles.textInput,
-            marginTop: 16,
-            paddingLeft: 30,
-            // borderColor: isInFocus ? "#FF6C00" : "#E8E8E8",
-            // backgroundColor: isInFocus ? "#FFFFFF" : "#F6F6F6",
-          }}
-          placeholderTextColor="#BDBDBD"
-          placeholder="Location..."
-        />
-        <SimpleLineIcons
-          style={styles.locationIcon}
-          name="location-pin"
-          size={24}
-          color="#BDBDBD"
-        />
-      </View>
-      <TouchableOpacity
-        disabled={!isPhotoDataReady}
-        activeOpacity={0.8}
-        onPress={postHandler}
-        style={{
-          ...styles.postBtn,
-          backgroundColor: isPhotoDataReady ? "#FF6C00" : "#F6F6F6",
-        }}
-      >
-        <Text
-          style={{
-            ...styles.postBtnText,
-            color: isPhotoDataReady ? "#FFFFFF" : "#BDBDBD",
+            ...styles.postBtn,
+            backgroundColor: isPhotoDataReady ? "#FF6C00" : "#F6F6F6",
           }}
         >
-          Publish
-        </Text>
-      </TouchableOpacity>
-    </View>
+          <Text
+            style={{
+              ...styles.postBtnText,
+              color: isPhotoDataReady ? "#FFFFFF" : "#BDBDBD",
+            }}
+          >
+            Publish
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
