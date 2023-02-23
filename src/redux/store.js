b70d6492679ac8389830f05b6870778807be28bc
Roman Serediuk <box4rom@gmail.com>
2023-02-23 09:28:33 +0200

    remove combine reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,6 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth/authReducer";
 
-const rootReducer = combineReducers({
-  [authSlice.name]: authSlice.reducer,
-});
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: { [authSlice.name]: authSlice.reducer },
 });
